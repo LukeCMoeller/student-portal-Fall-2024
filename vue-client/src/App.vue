5f504c1c39d4f7b5f1a13927f6665c446a6d03b7
jriddle11 <111756814+jriddle11@users.noreply.github.com>
2024-10-07 14:45:55 +0000

    josh com

--- vue-client/src/App.vue
+++ vue-client/src/App.vue
@@ -27,18 +27,3 @@ tokenStore.tryToken();
         </div>
     </UserProvider> 
 </template>
-<!-- old stuff in this comment 
-import Header from './components/layout/Header.vue';  
-import Footer from './components/layout/Footer.vue'; 
-import ApplicationForm from './components/forms/ApplicationForm.jsx';
-import ProfilesForm from './components/forms/ProfilesForm.jsx';
-import HomePage from './components/forms/HomePage.jsx';
-import AdminForm from './components/forms/AdminForm.jsx';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { UserProvider } from './context/UserContext.jsx'; 
-//import TicketForwarder from './utils/TicketForwarder.js';
-
-
-
--->
