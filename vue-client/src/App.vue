48013bb1169450769673825737b96679a4fc5b22
jriddle11 <111756814+jriddle11@users.noreply.github.com>
2024-10-10 07:51:35 +0000

    Styling

--- vue-client/src/App.vue
+++ vue-client/src/App.vue
@@ -14,13 +14,6 @@ import './App.css';
         <div className="appContainer"> 
           <Header/>
           <div className="contentBackground">
-            <nav>
-              <RouterLink to="/">Go to Home</RouterLink>
-              <RouterLink to="/error">Go to Error</RouterLink>
-              <RouterLink to="apply">Go to Apply</RouterLink>
-              <RouterLink to="/profile">Go to Profile</RouterLink>
-              <RouterLink to="/admin">Go to Admin</RouterLink>
-            </nav>
             <RouterView />
           </div> 
           <Footer/>
