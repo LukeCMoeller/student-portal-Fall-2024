ac616d86aabf97612d9793d89679a1c1abbc75e7
jriddle11 <111756814+jriddle11@users.noreply.github.com>
2024-10-11 17:32:29 +0000

    Profile form half

--- vue-client/src/components/forms/ProfilesForm.vue
+++ vue-client/src/components/forms/ProfilesForm.vue
@@ -1,5 +1,79 @@
-<script>
-</script>
 <template>
-    
+  <div :class="$style.ProfilesForm">
+    <div class="p-m-4">
+      <div class="p-grid p-justify-center p-m-4">
+        <div class="p-col-12 p-md-8" :class="$style.formContainer" style="background-color: #d1d1d1">
+          <h3 :class="$style.h3Style">Update your user profile:</h3>
+
+          <form @submit.prevent="submitForm">
+            <!-- First Name Field -->
+            <div class="p-field p-grid p-mb-3">
+                <label for="firstName" class="p-col-12 p-md-2">First Name</label>
+                <div class="p-col-12 p-md-10">
+                    <InputText id="firstName" v-model="firstName" placeholder="Enter your first name" />
+                </div>
+            </div>
+
+            <!-- Last Name Field -->
+            <div class="p-field p-grid p-mb-3">
+            <label for="lastName" class="p-col-12 p-md-2">Last Name</label>
+            <div class="p-col-12 p-md-10">
+                <InputText id="lastName" v-model="lastName" placeholder="Enter your last name" />
+            </div>
+            </div>
+
+            <!-- Email Field -->
+            <div class="p-field p-grid p-mb-3">
+            <label for="email" class="p-col-12 p-md-2">Email</label>
+            <div class="p-col-12 p-md-10">
+                <InputText id="email" v-model="email" placeholder="Enter your email" />
+            </div>
+            </div>
+
+            <!-- Submit Button -->
+            <div class="p-field p-grid p-mb-3">
+            <div class="p-col-12 p-md-10 p-offset-md-2">
+                <Button label="Submit" type="submit" class="p-button $style.btn-update" />
+            </div>
+            </div>
+        </form>
+        </div>
+      </div>
+    </div>
+
+    <footer :class="$style.footer">
+      <div class="p-m-4">
+        <p>CS Applications - Contact webmaster@cs.ksu.edu for help</p>
+      </div>
+    </footer>
+  </div>
 </template>
+
+<script>
+import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
+import styles from '../../styles/ProfileForm.module.css';
+import FloatLabel from 'primevue/floatlabel';
+
+export default {
+  name: 'ProfilesForm',
+  components: {
+    InputText,
+    Button,
+    FloatLabel,
+  },
+  setup() {
+    const firstName = '';
+    const lastName = '';
+    const email = '';
+    const wid = '';
+
+    return { firstName, lastName, email, wid };
+  },
+  computed: {
+    $style() {
+      return styles;
+    },
+  },
+};
+</script>
--- vue-client/src/main.js
+++ vue-client/src/main.js
@@ -1,10 +1,12 @@
 import './assets/main.css'
 import { createApp } from 'vue'
 import {createPinia} from 'pinia'
+import PrimeVue from 'primevue/config';
 import App from './App.vue'
 import router from './router'
 
 createApp(App)
+.use(PrimeVue)
 .use(router)
 .use(createPinia())
 .mount('#app')
