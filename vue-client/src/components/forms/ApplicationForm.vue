5c9c66a85a8eb91a4a40352f8a4a4c05d10c9724
jriddle11 <111756814+jriddle11@users.noreply.github.com>
2024-10-16 02:49:02 +0000

    Added DataTable

--- vue-client/src/components/forms/ApplicationForm.vue
+++ vue-client/src/components/forms/ApplicationForm.vue
@@ -12,13 +12,13 @@
                 <div class="grid">
                     <div class="col-4">
                         <FloatLabel variant="in">
-                        <InputText id="studentName" v-model="studentData.name" variant="filled" style="background-color: #d1d1d1; width: 20vw;"/>
+                        <InputText id="studentName" v-model="studentData.name" variant="filled" style="width: 20vw;"/>
                         <label for="studentName">Name:</label>
                         </FloatLabel>
                     </div>
                     <div class="col-4">
                         <FloatLabel variant="in">
-                        <InputText id="wid" v-model="studentData.wid" variant="filled" style="background-color: #d1d1d1; width: 20vw;"/>
+                        <InputText id="wid" v-model="studentData.wid" variant="filled" style="width: 20vw;"/>
                         <label for="wid">WID:</label>
                         </FloatLabel>
                     </div>
@@ -29,7 +29,7 @@
                         v-model="selectedAdvisor" 
                         :options="advisorOptions" 
                         showClear
-                        style="background-color: #d1d1d1; width: 20vw;" 
+                        style="width: 20vw;" 
                         />
                         <label for="advisor">Advisor:</label>
                         </FloatLabel>
@@ -42,6 +42,17 @@
                             <p>Any courses you are currently taking can be marked as <em>In Progress</em>. Any courses that you do not plan on taking need to be marked <em>Waiver Requested</em> and the reasons you are asking for the waiver must be explained below.</p>
                         </div>
                         </div>
+                    </div>
+
+                    <div class="col-12">
+                        <div :class="styles['custom-table-container']"> 
+                            <DataTable :value="courses" stripedRows class="styles.customTable">
+                                <Column field="class_descr" header="Course" />
+                                <Column field="course_id" header="Course ID" />
+                                <Column field="status" header="Status" />
+                                <Column field="grade" header="Grade" />
+                            </DataTable>
+                    </div>
                     </div>
                     
                     <div class="col-6">
@@ -73,13 +84,15 @@
   </template>
   
   <script>
-  import { ref} from 'vue';
+  import { shallowRef, ref } from 'vue';
   import InputText from 'primevue/inputtext';
   import Button from 'primevue/button';
   import Alert from 'primevue/message';
   import FloatLabel from 'primevue/floatlabel';
   import Select from 'primevue/select';
   import Textarea from 'primevue/textarea';
+  import DataTable from 'primevue/datatable';
+  import Column from 'primevue/column';
   import styles from '../../styles/ApplicationForm.module.css'; 
 
   
@@ -92,40 +105,46 @@
       Button,
       Alert,
       FloatLabel,
+      DataTable,
+      Column
     },
     setup() {
       const studentData = ref({wid: 1234, name: "Test Student"});
-      const courses = ref([]);
-      const loading = ref(false);
-      const statusMessage = ref('');
-      const alertStatus = ref('info');
-      const showAlert = ref(false);
-      const selectedAdvisor = ref('');
-      const courseUpdates = ref({});
-      const submitting = ref(false);
-      const additionalInfo = ref('');
+      const courses = shallowRef([
+        {class_descr: "Required", course_id: "CIS642", status: "Passed", grade: "A"},
+        {class_descr: "Required", course_id: "CIS505", status: "Passed", grade: "B"},
+        {class_descr: "Not Required", course_id: "CIS625", status: "Failed", grade: "D"},
+      ]);
+      const loading = shallowRef(false);
+      const statusMessage = shallowRef('');
+      const alertStatus = shallowRef('info');
+      const showAlert = shallowRef(false);
+      const selectedAdvisor = shallowRef('');
+      const courseUpdates = shallowRef({});
+      const submitting = shallowRef(false);
+      const additionalInfo = shallowRef('');
       const hardcodedGPA = "3.5";
   
-        const statusOptions = ref([
+        const statusOptions = [
         { value: "Complete", label: "Complete" },
         { value: "In-Progress", label: "In Progress" },
         { value: "transferred", label: "Transferred" },
         { value: "retaking", label: "Retaking" },
         { value: "waiver-requested", label: "Waiver Requested" },
-      ]);
+      ];
   
-        const gradeOptions = ref([
+        const gradeOptions = [
         { value: "n/a", label: "N/A" },
         { value: "A", label: "A" },
         { value: "B", label: "B" },
         { value: "C", label: "C" },
         { value: "D", label: "D" },
         { value: "F", label: "F"}
-        ]);
+        ];
     
-        const advisorOptions = ref([
+        const advisorOptions = [
             "Advisor1", "Advisor2"
-        ]);
+        ];
 
         return {styles, studentData, courses, loading, statusMessage, alertStatus, showAlert, selectedAdvisor, courseUpdates, submitting, additionalInfo, hardcodedGPA, advisorOptions}
     }
