085e3d5920c2147568e52dfd8bb1338ba55f4f9e
LukeCMoeller <111709091+LukeCMoeller@users.noreply.github.com>
2024-11-15 16:21:24 +0000

    moving discord to own folder

--- server/models/discord.js
+++ server/models/discord.js
@@ -1,39 +1,44 @@
-//Discord
-const { Client, GatewayIntentBits, Guild } = require('discord.js');
-const client = new Client({ intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMembers,
-] });
-let ourServerguild;
-let role;
-//KEEP SAFE   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-client.once('ready',async () => {
-  console.log('Bot is online!');
-  ourServerguild = client.guilds.cache.get('1285994775282978900'); // the server id
-  if (!ourServerguild) {
-    console.error('Guild not found!');
-    return;
-  }
-  role = ourServerguild.roles.cache.get('1285995785141485568'); //the role id
-  if (!role) {
-    console.error('Role not found!');
-    return;
-  }
-});
-client.on('guildMemberAdd', async (member) => {
-  if (!member) {
-    console.error('Member not found!');
-    return;
-  }
-  const welcomeChannel = member.guild.channels.cache.get('1306295646248239134');//channel id for welcome
-  if (welcomeChannel) {
-    welcomeChannel.send(`Welcome to the server, ${member.user.tag}! 🎉`);
-  }
-  try{
-    await member.roles.add(role);
-    console.log(`Role added from member: ${member.user.tag}`);
+const Model = require('./base.js')
+class discord extends Model {
+  static async runDiscordBot() {
+    const { Client, GatewayIntentBits, Guild } = require('discord.js');
+    const client = new Client({ intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+    ] });
+    let ourServerguild;
+    let role;
+    client.login("process.env.DISCORD_SECRET")
+    client.once('ready',async () => {
+      console.log('Bot is online!');
+      ourServerguild = client.guilds.cache.get('1285994775282978900'); // the server id
+      if (!ourServerguild) {
+        console.error('Guild not found!');
+        return;
+      }
+      role = ourServerguild.roles.cache.get('1285995785141485568'); //the role id
+      if (!role) {
+        console.error('Role not found!');
+        return;
+      }
+    });
+    client.on('guildMemberAdd', async (member) => { 
+      if (!member) {
+        console.error('Member not found!');
+        return;
+      }
+      const welcomeChannel = member.guild.channels.cache.get('1306295646248239134');//channel id for welcome
+      if (welcomeChannel) {
+        welcomeChannel.send(`Welcome to the server, ${member.user.tag}! 🎉`);
+      }
+      try{
+        await member.roles.add(role);
+        console.log(`Role added from member: ${member.user.tag}`);
 
-  }catch (error){
-    console.error('Error adding role:', error);
+      }catch (error){
+        console.error('Error adding role:', error);
+      }
+    });
   }
-});
+}
+module.exports = discord
--- server/server.js
+++ server/server.js
@@ -8,46 +8,14 @@ const cookieParser = require('cookie-parser');
 const session = require('./configs/session.js');
 const serverConfig = require('./configs/server');
 const knex = require('./configs/db');
+const discord = require('./models/discord.js');
 
-//Discord
-const { Client, GatewayIntentBits, Guild } = require('discord.js');
-const client = new Client({ intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMembers,
-] });
-let ourServerguild;
-let role;  
-//KEEP SAFE   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-client.once('ready',async () => {
-  console.log('Bot is online!');
-  ourServerguild = client.guilds.cache.get('1285994775282978900'); // the server id
-  if (!ourServerguild) {
-    console.error('Guild not found!');
-    return;
-  }
-  role = ourServerguild.roles.cache.get('1285995785141485568'); //the role id
-  if (!role) {
-    console.error('Role not found!');
-    return;
-  }
-});
-client.on('guildMemberAdd', async (member) => {
-  if (!member) {
-    console.error('Member not found!');
-    return;
-  }
-  const welcomeChannel = member.guild.channels.cache.get('1306295646248239134');//channel id for welcome
-  if (welcomeChannel) {
-    welcomeChannel.send(`Welcome to the server, ${member.user.tag}! 🎉`);
-  }
-  try{
-    await member.roles.add(role);
-    console.log(`Role added from member: ${member.user.tag}`);
 
-  }catch (error){
-    console.error('Error adding role:', error);
-  }
-});
+//runs the discord bot
+discord.runDiscordBot();
+
+
+
 
 const app = express();
 const PORT = serverConfig.port || 3001;
@@ -74,6 +42,7 @@ const applicationRoutes = require('./routes/applicationRoutes');
 const dataRoutes = require('./routes/dataRoutes');
 const authRoutes = require('./routes/auth'); // Renamed for clarity
 const adminRoutes = require('./routes/adminRoutes');
+//const discordRoutes = require('./routes/discordRoutes');
 
 // Use routes
 app.use('/api/v1', (req, res, next) => {console.log(req.path); next()}, apiRoutes);
@@ -82,6 +51,7 @@ app.use('/api', applicationRoutes);
 app.use('/api', dataRoutes);
 app.use('/auth', authRoutes);
 app.use('/api', adminRoutes);
+//app.use('/api', discordRoutes);
 
 // Catch 404 and forward to error handler
 app.use(function (req, res, next) {
