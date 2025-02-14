d29b8ae782265cd139eca91749fae87a8c8b6fd2
Luke Moeller <111709091+LukeCMoeller@users.noreply.github.com>
2025-02-14 23:27:31 +0000

    setup

--- server/linked-roles/discordbot.js
+++ server/linked-roles/discordbot.js
@@ -1,6 +1,7 @@
 const Model = require('../models/base.js')
 //const crypto = require('crypto')
 
+
 class discord extends Model {
   static async runDiscordBot() {
     const { Client, GatewayIntentBits, Guild } = require('discord.js');
@@ -8,9 +9,17 @@ class discord extends Model {
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMembers,
     ] });
+    let TestingDatabase = false;
+    let Testing100 = false;
+    let Testing200 = false;
     let ourServerguild;
-    let role;
-    const uniqueId = crypto.randomUUID();
+    let roles = [
+      '1308820949310902282', // Student role
+      '1340083275179491328', // CIS 100 student
+      '1340083455555276821'  // CIS 200 student
+    ];
+    let hasRoles = new Array(roles.length).fill(false);
+    //const uniqueId = crypto.randomUUID(); handled elsewhere
     //console.log("nexline");
     //console.log(uniqueId);
     client.login(process.env.DISCORD_SECRET);
@@ -21,10 +30,12 @@ class discord extends Model {
         console.error('Guild not found!');
         return;
       }
-      role = ourServerguild.roles.cache.get('1308820949310902282'); //the role id
-      if (!role) {
-        console.error('Role not found!');
-        return;
+      for(let i = 0; i < roles.length; i++){
+        roles[i] = ourServerguild.roles.cache.get(roles[i]);
+        if(!roles[i]){
+          console.error('Role not found!');
+          return;
+        }
       }
     });
     client.on('guildMemberAdd', async (member) => { 
@@ -36,14 +47,34 @@ class discord extends Model {
       if (welcomeChannel) {
         welcomeChannel.send(`Welcome to the server, ${member.user.tag}! 🎉`);
       }
+      const username = member.user.username;
+      if(TestingDatabase){
+        for(let i = 0; i < roles.length; i++){
+        if(username){//discord username is in database
+          let classes; //somehow get a list of every class the user has 
+          for(let i = 1; i < roles.length; i++){
+            if(hasRoles[i]){
+              roles[i] = ourServerguild.roles.cache.get(roles[i]);
+            }
+          }
+        }else{
+          //maybe give them role that limits everything
+        }
+      }
       try{
-        await member.roles.add(role);
-        console.log(`Role added from member: ${member.user.tag}`);
-
+        await member.roles.add(roles[0]);
+        if(Testing100){
+          await member.roles.add(ourServerguild.roles.cache.get(roles[1]));
+        }
+        if(Testing200){
+          await member.roles.add(ourServerguild.roles.cache.get(roles[2]));
+        }
+        console.log(`Roles added from member: ${member.user.tag}`);
       }catch (error){
         console.error('Error adding role:', error);
       }
     });
+
   }
 }
 module.exports = discord
