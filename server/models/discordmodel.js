e57c081081f65d73941ba3b72d71dfde868a9a3b
Luke Moeller <111709091+LukeCMoeller@users.noreply.github.com>
2025-02-26 19:35:28 +0000

    pushing to branch

--- server/models/discordmodel.js
+++ server/models/discordmodel.js
@@ -25,17 +25,14 @@ class User extends Model {
         console.error('Error fetching users with Discord:', err);
       }
     }
-    //HEEEEEEEEEEEEEY
-    //HEEYEHEYEHEY
-    //checked the database dirrectly and theres a TON of user_table stuff but no course_student stuff. maybe it dosnt acually exist
-    async get_student_courses(discord_id){//more complete query
+    async get_student_courses(discord_id){
       try {
-        const stuff = await knex('course_students') //says "relation course_students dose not exist"
+        const stuff = await knex('course_students') 
           .join('users', 'course_students.user_id', '=', 'users.id')
           .join('user_discord', 'users.id', '=', 'user_discord.user_id')
           .join('courses', 'course_students.course_id', '=', 'courses.id')
-          .where('user_discord.discord_id', discord_id)
-          .where('courses.subject', 'like', '%CIS%') //needs to be tested to confirm
+          //.where('user_discord.discord_id', discord_id)
+          //.where('courses.subject', 'like', '%CIS%')
           .select('courses.catalog'); //only need this
           console.log("testing below"); //log isnt hitting
           console.log(stuff);
@@ -45,61 +42,6 @@ class User extends Model {
         throw err;
       }
     }
-    /*
-     //one that goes through every table
-  static get relationMappings() {
-    return {
-      roles: {
-        relation: Model.ManyToManyRelation,
-        modelClass: User,
-        join: {
-          from: 'user_discord.discord_id',
-          // ManyToMany relation needs the `through` object
-          // to describe the join table.
-          through: {
-            // If you have a model class for the join table
-            // you need to specify it like this:
-            // modelClass: PersonMovie,
-            from: 'user_discord.discord_id',
-            to: 'users.id',//? where does  discord id go here
-          },
-          through: {
-            // If you have a model class for the join table
-            // you need to specify it like this:
-            // modelClass: PersonMovie,
-            from: 'users.id',
-            to: 'course_students.user_id',
-          },
-          to: 'course_students.user_id',
-        },
-        filter: (builder) => builder.select('User'),
-      },
-    }
-  }*/
- /*
-     // one that goes a single time from spot to spot
-  static get relationMappings() {
-    return {
-      roles: {
-        relation: Model.ManyToManyRelation,
-        modelClass: User,
-        join: {
-          from: 'user_discord.user_id',
-          // ManyToMany relation needs the `through` object
-          // to describe the join table.
-          through: {
-            // If you have a model class for the join table
-            // you need to specify it like this:
-            // modelClass: PersonMovie,
-            from: 'user_discord.user_id',
-            to: 'course_students.user_id',
-          },
-          to: 'course_students.user_id',
-        },
-        filter: (builder) => builder.select('User'),
-      },
-    }
-  }*/
 }  
 
 module.exports = User;
